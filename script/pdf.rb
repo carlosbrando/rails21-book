f63275bdf86de17cd839ee52a0e6030aadfd4e16
Carlos Brando <eduardobrando@gmail.com>
2008-06-02 17:55:02 -0300

    Muda a formatação.

--- script/pdf.rb
+++ script/pdf.rb
@@ -1,19 +1,9 @@
-# def get_css(html_string)
-#   css = []
-#   html_string.each do |line|
-#     if line =~ /.*<link.*href="..(\/layout\/.*\.css)".*type="text\/css".*\/>.*/
-#       css << "'#{$1}'"
-#     end
-#   end
-#   css.join(", ")
-# end
-
 desc 'Cria um arquivo pdf à partir do html gerado'
 task :pdf => :html do
   prince = Prince.new()
   html_string = File.new("output/index.html").read
-  prince.add_style_sheets 'layout/default.css'
-  # get_css(html_string) 
+  # prince.add_style_sheets 'layout/default.css'
+  prince.add_style_sheets 'layout/second.css'
   
   File.open('output/book.pdf', 'w') do |f|
     f.puts prince.pdf_from_string(html_string)
@@ -21,5 +11,3 @@ task :pdf => :html do
   
   `open output/book.pdf`
 end
-
-
