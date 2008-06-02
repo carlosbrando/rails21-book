0eba0f8a0b0bab385cd751370c081e9c50c12b49
Carlos Brando <eduardobrando@gmail.com>
2008-06-02 16:47:01 -0300

    Ajusta a formatação.

--- script/pdf.rb
+++ script/pdf.rb
@@ -1,18 +1,19 @@
-def get_css(html_string)
-  css = []
-  html_string.each do |line|
-    if line =~ /.*<link.*href="..(\/layout\/.*\.css)".*type="text\/css".*\/>.*/
-      css << "'#{$1}'"
-    end
-  end
-  css.join(", ")
-end
+# def get_css(html_string)
+#   css = []
+#   html_string.each do |line|
+#     if line =~ /.*<link.*href="..(\/layout\/.*\.css)".*type="text\/css".*\/>.*/
+#       css << "'#{$1}'"
+#     end
+#   end
+#   css.join(", ")
+# end
 
 desc 'Cria um arquivo pdf à partir do html gerado'
 task :pdf => :html do
   prince = Prince.new()
   html_string = File.new("output/index.html").read
-  prince.add_style_sheets get_css(html_string) 
+  prince.add_style_sheets 'layout/default.css'
+  # get_css(html_string) 
   
   File.open('output/book.pdf', 'w') do |f|
     f.puts prince.pdf_from_string(html_string)
--- text/0/0_Title.markdown
+++ text/0/0_Title.markdown
@@ -1,1 +1,18 @@
-# Ruby on Rails 2.1 - O que tem de novo?
+#Introdução
+
+Por volta do mês de julho de 2004 David Heinemeier Hansson lançou publicamente o framework Ruby on Rails, que havia sido extraído de um software chamado Basecamp. Mais de três anos depois, no dia 7 de dezembro de 2007 o Ruby on Rails chegou a sua versão 2.0 com diversas alterações importantes.
+
+De lá para cá se passaram seis meses, e neste tempo mais de 1400 programadores do mundo todo contribuiram criando 1600 patches. E hoje, 1 de junho de 2008 o Ruby on Rails chega à sua versão 2.1.
+
+De acordo com David as principais novidades nesta versão são:
+
+* Timezones
+* Dirty tracking
+* Gem Dependencies
+* Named scope
+* UTC-based migrations
+* Better caching
+
+Para atualizar ou instalar a nova versão, é o de sempre:
+
+	gem install rails
