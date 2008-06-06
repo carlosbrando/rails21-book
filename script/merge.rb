3c5158e40e300fc0ee2833b9ba9c525724f7329d
Nando Vieira <fnando@kernelpanic.local>
2008-06-05 23:24:57 -0300

    - alterado o texto "o que à de novo" para "o que tem de novo?", conforme nota no header; a forma correta é "o que há de novo" - removida recursão no merge dos arquivos - corrigida numeração dos capítulos

--- script/merge.rb
+++ script/merge.rb
@@ -1,24 +1,8 @@
 desc 'Agrupa todos os arquivos em um único arquivo'
 task :merge do
-  File.open('output/full_book.texttile', 'w') do |f|
-    directory "text", f
-  end
-end
-
-def directory(dir_name, output)
-  dir = Dir.new(dir_name)
-  dir.each do |file_name|
-    unless file_name =~ /^\./
-      file = "#{dir_name}/#{file_name}"
-      if File.stat(file).directory?
-        directory(file, output)
-      else
-        merge_output(file, output)
-      end 
+  File.open('output/full_book.texttile', 'w+') do |f|
+    Dir["text/**/*.markdown"].sort.each do |path|
+      f << File.new(path).read + "\r\n"
     end
   end
-end
-
-def merge_output(file, output)
-  output.puts File.new(file).read + "\n\r"
 end
